3e057b470df1669114082f9edd254b287c7fb4ef
Patrick Hanford <patrick.hanford@dcsg.com>
2018-10-19 17:32:34 -0400

    Changed zoom value from 5 to 7 when searching to avoid cluster collision when a new marker is added

--- src/js/map.js
+++ src/js/map.js
@@ -211,7 +211,7 @@ $.getJSON("https://api.myjson.com/bins/i1k4o", function (storeData) {
                     let c = m.getLatLng();
                     let point = new L.LatLng(c.lat, c.lng);
                    
-                    map.setView(point, 5, {
+                    map.setView(point, 7, {
                         animation: true
                     })
                     map.on('popupclose', function () {
